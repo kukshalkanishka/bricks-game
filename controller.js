090653152c63b74bb972b90c7cb30856fbcf5ea6
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2019-01-11 14:26:13 +0530

    Add right and left movemenet to paddle.

--- controller.js
+++ controller.js
@@ -1,26 +1,45 @@
+const ARROW_LEFT = 'ArrowLeft';
+const ARROW_RIGHT = 'ArrowRight';
+
 const addPixelSuffix = function(element) {
   return element + 'px';
 };
 
-const drawPaddle = function(paddle, paddleDiv) {
+const drawPaddle = function(document, paddle) {
+  const paddleDiv = document.getElementById('paddle_1');
   paddleDiv.style.width = addPixelSuffix(paddle.width);
   paddleDiv.style.height = addPixelSuffix(paddle.height);
   paddleDiv.style.left = addPixelSuffix(paddle.left);
   paddleDiv.style.bottom = addPixelSuffix(paddle.bottom);
 };
 
-const createPaddle = function(paddle) {
-  const screen = document.getElementById('screen');
+const getScreen = document => document.getElementById('screen');
+
+const createPaddle = function(document, paddle) {
+  const screen = getScreen(document);
   const paddleDiv = document.createElement('div');
   paddleDiv.id = 'paddle_1';
   paddleDiv.className = 'paddle';
   screen.appendChild(paddleDiv);
-  drawPaddle(paddle, paddleDiv);
+  drawPaddle(document, paddle);
+};
+
+const handleMovement = function(document, paddle) {
+  if (event.key == ARROW_LEFT) {
+    paddle.moveLeft();
+  }
+  if (event.key == ARROW_RIGHT) {
+    paddle.moveRight();
+  }
+  drawPaddle(document, paddle);
 };
 
 const initialiseGame = function() {
   const paddle = new Paddle(100, 20, 430, 10);
-  createPaddle(paddle);
+  createPaddle(document, paddle);
+  const screen = getScreen(document);
+  screen.focus();
+  screen.onkeydown = handleMovement.bind(null, document, paddle);
 };
 
 window.onload = initialiseGame;
--- paddle.js
+++ paddle.js
@@ -1,8 +1,15 @@
 class Paddle {
-  constructor(width, height, left, bottom) {
+  constructor(width, height, left, bottom, speed = 10) {
     this.width = width;
     this.height = height;
     this.left = left;
     this.bottom = bottom;
+    this.speed = speed;
+  }
+  moveLeft() {
+    this.left = this.left - this.speed;
+  }
+  moveRight() {
+    this.left = this.left + this.speed;
   }
 }
